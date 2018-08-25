74a83e389edfdf633c699ffa96e06d16f2143384
Özkan Yilmaz <ozkanyilmaz@gmail.com>
2018-08-25 19:24:41 +0300

    Listem Codable Plist dosyalı versiyonu baseline a alındı

---
+++ Listem-CoreData/Controllers/ToDoListViewController.swift
@@ -0,0 +1,90 @@
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    var itemArray = [Item]()
+    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadItems()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        
+        cell.accessoryType = itemArray[indexPath.row].done ? .checkmark : .none
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        saveItems()
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textView = UITextField()
+        textView.placeholder = "Görev adı"
+        
+        let alert = UIAlertController(title: "Yeni görev ekle", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Ekle", style: .default) { (action) in
+            
+            let newItem = Item()
+            newItem.title = textView.text!
+            
+            self.itemArray.append(newItem)
+            
+            self.saveItems()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            textView = alertTextField
+        }
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            
+            try data.write(to: dataFilePath!)
+            
+        } catch {
+            print("Error encodind & writing item array into plist file")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array, \(error)")
+            }
+        }
+        
+    }
+}
+
---
+++ Listem-CoreData/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Listem
+//
+//  Created by Ozkan Yilmaz on 25.08.2018.
+//  Copyright © 2018 Ozkan Yilmaz. All rights reserved.
+//
+
+import Foundation
+
+class Item : Codable {
+    var title : String = ""
+    var done : Bool = false
+}
